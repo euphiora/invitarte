a490205009ba0a267dd79410147ff59fd08296da
euphiora <124705617+euphiora@users.noreply.github.com>
2025-07-25 20:06:51 -0300

    Header y Banner

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,16 +1,37 @@
 "use client";
 
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Header() {
-
   return (
-    <header className="w-full bg-white shadow-md py-4 px-8">
-      <nav className="flex justify-between items-center">
-        <Link href="/" className="text-xl font-bold text-gray-800 hover:text-blue-600 transition-colors">
-          Invitarte
-        </Link>
-      </nav>
+    <header className="w-full bg-accent shadow-md py-4 px-8 flex items-center justify-between">
+      <Link
+        href="/"
+        className="text-xl font-bold text-gray-800 transition-colors"
+      >
+        <Image
+          src="/logo.jpg"
+          alt="Logo"
+          height={100}
+          width={100}
+        />
+      </Link>
+
+      <div className="flex gap-3">
+        <a href="#" className="hover:text-gray-900">
+          Home
+        </a>
+        <a href="#" className="hover:text-gray-900">
+          Productos
+        </a>
+        <a href="#" className="hover:text-gray-900">
+          Cómo comprar
+        </a>
+        <a href="#" className="hover:text-gray-900">
+          Contacto
+        </a>
+      </div>
     </header>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,30 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default async function Home() {
-  return <div>hola prueba vercel</div>;
+  return (
+    <div className="relative isolate overflow-hidden py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:mx-0">
+          <h2 className="flex text-5xl font-semibold sm:text-7xl flex-col">
+            <span>Bienvenido</span> <span>a Invitarte</span>
+          </h2>
+          <p className="mt-8 text-lg font-medium text-pretty sm:text-xl/8">
+            Diseñamos detalles digitales listos para hacer de cada momento algo
+            especial.
+          </p>
+        </div>
+        <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
+          <div className="grid grid-cols-1 gap-x-8 gap-y-6 text-base/7 font-semibold sm:grid-cols-2 md:flex lg:gap-x-10">
+            <Button asChild variant="default">
+              <Link href="#">Ver productos</Link>
+            </Button>
+            <Button asChild variant="outline">
+              <Link href="#">¿Cómo comprar?</Link>
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
